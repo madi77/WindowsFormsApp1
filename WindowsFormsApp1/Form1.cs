1cb256f64d5240a4899323e5f9c2fb0e91d4153c
Marinel <info@mdshop.se>
2020-10-13 21:22:45 +0200

    Add project files.

---
+++ WindowsFormsApp1/Form1.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            
+
+            var selectedItemToPass = listBox1.SelectedItem.ToString();
+
+            //Form2 myform = new Form2(selectedItemToPass);
+
+            Form2 form2 = new Form2();
+            form2.PassValue(selectedItemToPass);
+            form2.Show();
+            
+        }
+
+      
+    }
+}
---
+++ WindowsFormsApp1/Form2.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form2 : Form
+    {
+        public Form2()
+        {
+            InitializeComponent();
+        }
+
+        string selectedItem = String.Empty;
+
+        public Form2(string value)
+        {
+            InitializeComponent();
+            selectedItem = value;
+        }
+
+
+
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+           textBox1.Text = selectedItem;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            this.Hide();
+        }
+
+        public void PassValue(string strValue)
+        {
+            textBox1.Text = "dcsdc";
+        }
+    }
+}
